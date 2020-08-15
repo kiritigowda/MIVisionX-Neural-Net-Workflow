440268b56a5c4a55949e4d1b36223960746ee21b
Hansel Yang <hanselyang123@gmail.com>
2020-08-14 17:48:18 -0700

    python version

--- scripts/inference_control.py
+++ scripts/inference_control.py
@@ -1,9 +1,10 @@
 import os
-from PyQt4 import QtGui, uic
+#from PyQt4 import QtGui, uic
+from PyQt5 import QtWidgets, uic
 #from inference_viewer import *
 from rali_training_setup import *
 
-class InferenceControl(QtGui.QMainWindow):
+class InferenceControl(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(InferenceControl, self).__init__(parent)
         self.runningState = False
@@ -52,30 +53,30 @@ class InferenceControl(QtGui.QMainWindow):
 
     def browseFile(self):
         if self.format_comboBox.currentText() == 'nnef':
-            self.file_lineEdit.setText(QtGui.QFileDialog.getExistingDirectory(self, 'Open Folder', './'))    
+            self.file_lineEdit.setText(QtWidgets.QFileDialog.getExistingDirectory(self, 'Open Folder', './'))    
         else:
-            self.file_lineEdit.setText(QtGui.QFileDialog.getOpenFileName(self, 'Open File', './', '*'))
+            self.file_lineEdit.setText(QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', './', '*'))
 
     def browseDPath(self):
-        self.dpath_lineEdit.setText(QtGui.QFileDialog.getExistingDirectory(self, 'Open Folder', './'))
+        self.dpath_lineEdit.setText(QtWidgets.QFileDialog.getExistingDirectory(self, 'Open Folder', './'))
 
     def browseOPath(self):
-        self.opath_lineEdit.setText(QtGui.QFileDialog.getExistingDirectory(self, 'Open Folder', './'))
+        self.opath_lineEdit.setText(QtWidgets.QFileDialog.getExistingDirectory(self, 'Open Folder', './'))
 
     def browseOutput(self):
-        self.output_lineEdit.setText(QtGui.QFileDialog.getExistingDirectory(self, 'Open Folder', './'))
+        self.output_lineEdit.setText(QtWidgets.QFileDialog.getExistingDirectory(self, 'Open Folder', './'))
 
     def browseLabel(self):
-        self.label_lineEdit.setText(QtGui.QFileDialog.getOpenFileName(self, 'Open File', './', '*.txt'))
+        self.label_lineEdit.setText(QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', './', '*.txt'))
 
     def browseImage(self):
-        self.image_lineEdit.setText(QtGui.QFileDialog.getExistingDirectory(self, 'Open Folder', './'))
+        self.image_lineEdit.setText(QtWidgets.QFileDialog.getExistingDirectory(self, 'Open Folder', './'))
 
     def browseVal(self):
-        self.val_lineEdit.setText(QtGui.QFileDialog.getOpenFileName(self, 'Open File', './', '*.txt'))
+        self.val_lineEdit.setText(QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', './', '*.txt'))
 
     def browseHier(self):
-        self.hier_lineEdit.setText(QtGui.QFileDialog.getOpenFileName(self, 'Open File', './', '*.csv'))
+        self.hier_lineEdit.setText(QtWidgets.QFileDialog.getOpenFileName(self, 'Open File', './', '*.csv'))
 
     def readSetupFile(self):
         setupDir = '~/.mivisionx-validation-tool'
