d127ce8096bbcc9d1d0b2703aaccf3376a487f2c
Hansel Yang <hanselyang123@gmail.com>
2020-08-24 18:25:03 -0700

    trining fix

--- scripts/rali_training_setup.py
+++ scripts/rali_training_setup.py
@@ -249,7 +249,7 @@ class modelTraining(QtCore.QObject):
         self.dataset_train = datapath + '/train'
         self.dataset_val = datapath + '/val'
         
-        self.crop = (int)(modelInputDims.split(',')[2])
+        self.crop = (int)(input_dims.split(',')[2])
         self.net = None
         #self.train_loader = None
         #self.val_loader = None
