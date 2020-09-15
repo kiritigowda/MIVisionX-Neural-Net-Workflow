0e1b6a34efdd1ce767ac6ed52d2c6458d829137e
Hansel Yang <hanselyang123@gmail.com>
2020-09-14 17:04:04 -0700

    gui improvements

--- scripts/train_viewer.py
+++ scripts/train_viewer.py
@@ -38,47 +38,26 @@ class TrainViewer(QtGui.QMainWindow):
             self.model_progressBar.setMinimum(0)
             self.model_progressBar.setMaximum(self.epoch)
             self.mProg_label.setText("Epoch %d of %d" % (0, self.epoch))
-            # for image in self.image_list:
-            #     count = count + 1
-            #     print (image)
-            # print(count)
-            # self.container_index = (int)(container_logo)
-            # self.cpu_name = cpu_name
-            # self.gpu_name = gpu_name
-            # self.pauseState = False
-            # self.showAug = False
-            # self.elapsedTime = QTime.currentTime()
-            # self.lastTime = 0
-            # self.totalElapsedTime = 0.0
-            # self.totalAccuracy = 0
-            # self.progIndex = 0
-            # self.augIntensity = 0.0
-            # self.imgCount = 0
-            # self.frameCount = 9
-            # self.lastIndex = self.frameCount - 1
+            self.frameCount = 8
+
+            self.imgCount = self.frameCount
+            self.lastIndex = self.frameCount - 1
             self.epochs = [0] 
-            self.loss = [0]
+            self.loss = [4]
             self.top1 = [0]
             self.top5 = [0]
-            # self.totalCurve = None
-            # self.augCurve = None
+
             self.lgraph = None
             self.agraph = None
+            self.t1Curve = None
+            self.t5Curve= None
             self.legend = None
-            # self.pen = pg.mkPen('w', width=4)
-            # self.AMD_Radeon_pixmap = QPixmap("./data/images/AMD_Radeon.png")
-            # self.AMD_Radeon_white_pixmap = QPixmap("./data/images/AMD_Radeon-white.png")
-            # self.MIVisionX_pixmap = QPixmap("./data/images/MIVisionX-logo.png")
-            # self.MIVisionX_white_pixmap = QPixmap("./data/images/MIVisionX-logo-white.png")
-            # self.EPYC_pixmap = QPixmap("./data/images/EPYC-blue.png")
-            # self.EPYC_white_pixmap = QPixmap("./data/images/EPYC-blue-white.png")
-            # self.docker_pixmap = QPixmap("./data/images/Docker.png")
-            # self.singularity_pixmap = QPixmap("./data/images/Singularity.png")
-            # self.rali_pixmap = QPixmap("./data/images/RALI.png")
-            # self.rali_white_pixmap = QPixmap("./data/images/RALI-white.png")
-            # self.graph_image_pixmap = QPixmap("./data/images/Graph-image.png")
-            # self.graph_image_white_pixmap = QPixmap("./data/images/Graph-image-white.png")
-            
+    
+            pixmap = QPixmap(next(self.it)).scaled(100,100)
+            self.image_frame.setPixmap(pixmap)
+            self.loss_label.setStyleSheet("color: red")
+            self.top1_label.setStyleSheet("color: blue")
+            self.top5_label.setStyleSheet("color: green")
             self.initGraph()
             self.updateTimer = QTimer()
             self.snoopTimer = QTimer()
@@ -86,40 +65,47 @@ class TrainViewer(QtGui.QMainWindow):
             self.updateTimer.timeout.connect(self.showImage)
             self.snoopTimer.timeout.connect(self.getEpochValues)
             self.updateTimer.start(100)
-            self.snoopTimer.start(100)
+            self.snoopTimer.start(300)
        
     def initGraph(self):
-        # self.setStyleSheet("background-color: white")
-        self.lgraph = pg.PlotWidget(title="Loss vs Epoch")
-        self.lgraph.setLabel('left', 'Loss')
+        #self.setStyleSheet("background-color: white")
+        self.lgraph = pg.PlotWidget()
+        self.lgraph.setLabel('left', 'Loss(%)')
         self.lgraph.setLabel('bottom', 'Epoch(s)')
         self.lgraph.setXRange(0, self.epoch+1, padding=0)
-        self.lgraph.setYRange(0, 4, padding=0)
+        self.lgraph.setYRange(0, 5.5, padding=0)
 
-        self.agraph = pg.PlotWidget(title="Accuracy vs Epoch")
-        self.agraph.setLabel('left', 'Accuracy(top1)')
+        self.agraph = pg.PlotWidget()
+        self.agraph.setLabel('left', 'Accuracy(%)')
         self.agraph.setLabel('bottom', 'Epoch(s)')
         self.agraph.setXRange(0, self.epoch+1, padding=0)
-        self.agraph.setYRange(0, 80, padding=0)
+        self.agraph.setYRange(0, 105, padding=0)
 
         pg.setConfigOptions(antialias=True)
         
-        self.lcurve = self.lgraph.plot(pen=pg.mkPen('b', width=4))
-        self.acurve = self.agraph.plot(pen=pg.mkPen('r', width=4))
         self.lgraph.setBackground(None)
-        self.lgraph.setMaximumWidth(500)
-        self.lgraph.setMaximumHeight(300)
+        self.lgraph.getAxis('left').setPen(pg.mkPen('k', width=2))
+        self.lgraph.getAxis('left').setTextPen(pg.mkPen('k', width=2))
+        self.lgraph.getAxis('bottom').setPen(pg.mkPen('k', width=2))
+        self.lgraph.getAxis('bottom').setTextPen(pg.mkPen('k', width=2))
         self.agraph.setBackground(None)
-        self.agraph.setMaximumWidth(500)
-        self.agraph.setMaximumHeight(300)        
-        self.legend = pg.LegendItem(offset=(370,1))
-        self.legend.setParentItem(self.lgraph.plotItem)
+        self.agraph.getAxis('left').setPen(pg.mkPen('k', width=2))
+        self.agraph.getAxis('left').setTextPen(pg.mkPen('k', width=2))
+        self.agraph.getAxis('bottom').setPen(pg.mkPen('k', width=2))        
+        self.agraph.getAxis('bottom').setTextPen(pg.mkPen('k', width=2))        
+        self.lcurve = self.lgraph.plot(pen=pg.mkPen('r', width=4))
+        self.t1Curve = self.agraph.plot(pen=pg.mkPen('b', width=4))
+        self.t5Curve = self.agraph.plot(pen=pg.mkPen('g', width=4))
+        self.legend = pg.LegendItem(offset=(420,5), labelTextColor='k')
         self.legend.setParentItem(self.agraph.plotItem)
+        self.legend.addItem(self.t1Curve, 'Top1')
+        self.legend.addItem(self.t5Curve, 'Top5')
         self.verticalLayout.addWidget(self.lgraph)
         self.verticalLayout_2.addWidget(self.agraph)
-
+    
     def initEngines(self):
         self.receiver_thread = QThread()
+
         # Creating an object for train
         self.trainEngine = modelTraining(self.model, self.datapath, self.PATH, self.training_device, self.num_gpu, self.batch_size, self.epoch, self.rali_cpu, self.input_dims, self.num_thread)
         
@@ -144,122 +130,22 @@ class TrainViewer(QtGui.QMainWindow):
             self.top5.append(top5)
             self.plotGraph()
 
-    # def paintEvent(self, event):
-    #     pixmap = QPixmap(next(self.it)).scaled(200,200)
-    #     self.image_frame.setPixmap(pixmap)
-        #self.showAugImage()
-        #self.showImage()
-        #self.displayFPS()
-        # if self.imgCount == self.total_images:
-        #     if self.loop == 'yes':
-        #         self.resetViewer()
-        #     else:
-        #        self.terminate()
-    
     def showImage(self):
-        pixmap = QPixmap(next(self.it)).scaled(200,200)
-        self.image_frame.setPixmap(pixmap)
-    # def resetViewer(self):
-    #     self.imgCount = 0
-    #     del self.x[:]
-    #     self.x.append(0)
-    #     del self.y[:]
-    #     self.y.append(0)
-    #     del self.augAccuracy[:]
-    #     for augmentation in range(self.batch_size_int):
-    #         self.augAccuracy.append([0])
-
-    #     self.lastTime = 0
-    #     self.elapsedTime = QTime.currentTime()
-    #     self.totalElapsedTime = 0.0
-    #     self.progIndex = 0
-    #     self.showAug = False
-    #     self.lastIndex = self.frameCount - 1
-    #     self.totalCurve.clear()
-    #     self.augCurve.clear()
-    #     self.name_label.setText("Model: %s" % (self.model_name))
-    #     self.legend.removeItem(self.lastAugName)
+        index = self.imgCount % self.frameCount
+        pixmap = QPixmap(next(self.it)).scaled(100,100)
+        self.image_layout.itemAt(index).widget().setPixmap(pixmap)
+        self.image_layout.itemAt(index).widget().setStyleSheet("border: 5px solid darkred;");
+        self.image_layout.itemAt(self.lastIndex).widget().setStyleSheet("border: 0");
+        self.imgCount += 1
+        self.lastIndex = index
 
     def plotGraph(self):
         self.lcurve.setData(x=self.epochs, y=self.loss)
-        self.acurve.setData(x=self.epochs, y=self.top1)
-        # for augmentation in range(self.batch_size_int):
-        #     augStats = self.inferenceEngine.getAugStats(augmentation)
-        #     top5 = augStats[1]
-        #     mis = augStats[2]
-        #     totalCount = top5 + mis
-        #     totalAccuracy = (float)(top5) / (totalCount+1) * 100
-        #     self.augAccuracy[augmentation].append(totalAccuracy)
+        self.t1Curve.setData(x=self.epochs, y=self.top1)
+        self.t5Curve.setData(x=self.epochs, y=self.top5)
     
-        # if self.showAug:
-        #     self.augCurve.setData(x=self.x, y=self.augAccuracy[self.progIndex], pen=pg.mkPen('b', width=4))
-
-    # def setBackground(self):
-    #     if self.dark_checkBox.isChecked():
-    #         self.setStyleSheet("background-color: #25232F;")
-    #         self.pen = pg.mkPen('w', width=4)
-    #         self.graph.setBackground(None)
-    #         self.origTitle_label.setStyleSheet("color: #C82327;")
-    #         self.controlTitle_label.setStyleSheet("color: #C82327;")
-    #         self.progTitle_label.setStyleSheet("color: #C82327;")
-    #         self.graphTitle_label.setStyleSheet("color: #C82327;")
-    #         self.augTitle_label.setStyleSheet("color: #C82327;")
-    #         self.name_label.setStyleSheet("color: white;")
-    #         self.dataset_label.setStyleSheet("color: white;")
-    #         self.imgProg_label.setStyleSheet("color: white;")
-    #         self.fps_label.setStyleSheet("color: #C82327;")
-    #         self.dark_checkBox.setStyleSheet("color: white;")
-    #         self.verbose_checkBox.setStyleSheet("color: white;")
-    #         self.rali_checkBox.setStyleSheet("color: white;")
-    #         self.level_label.setStyleSheet("color: white;")
-    #         self.low_label.setStyleSheet("color: white;")
-    #         self.high_label.setStyleSheet("color: white;")
-    #         self.cpu_label.setStyleSheet("color: #C82327;")
-    #         self.gpu_label.setStyleSheet("color: #C82327;")
-    #         self.cpuName_label.setStyleSheet("color: white;")
-    #         self.gpuName_label.setStyleSheet("color: white;")
-    #         self.AMD_logo.setPixmap(self.AMD_Radeon_white_pixmap)
-    #         if self.rali_checkBox.isChecked():
-    #             self.MIVisionX_logo.setPixmap(self.rali_white_pixmap)
-    #             self.graph_imageLabel.setPixmap(self.graph_image_white_pixmap)
-    #         else:
-    #             self.MIVisionX_logo.setPixmap(self.MIVisionX_white_pixmap)
-    #         self.EPYC_logo.setPixmap(self.EPYC_white_pixmap)
-    #         self.totalCurve.setData(x=self.x, y=self.y, pen=self.pen)
-    #     else:
-    #         self.setStyleSheet("background-color: white;")
-    #         self.pen = pg.mkPen('k', width=4)
-    #         self.graph.setBackground(None)
-    #         self.origTitle_label.setStyleSheet("color: 0;")
-    #         self.controlTitle_label.setStyleSheet("color: 0;")
-    #         self.progTitle_label.setStyleSheet("color: 0;")
-    #         self.graphTitle_label.setStyleSheet("color: 0;")
-    #         self.augTitle_label.setStyleSheet("color: 0;")
-    #         self.name_label.setStyleSheet("color: 0;")
-    #         self.dataset_label.setStyleSheet("color: 0;")
-    #         self.imgProg_label.setStyleSheet("color: 0;")
-    #         self.fps_label.setStyleSheet("color: 0;")
-    #         self.dark_checkBox.setStyleSheet("color: 0;")
-    #         self.verbose_checkBox.setStyleSheet("color: 0;")
-    #         self.rali_checkBox.setStyleSheet("color: 0;")
-    #         self.level_label.setStyleSheet("color: 0;")
-    #         self.low_label.setStyleSheet("color: 0;")
-    #         self.high_label.setStyleSheet("color: 0;")
-    #         self.cpu_label.setStyleSheet("color: 0;")
-    #         self.gpu_label.setStyleSheet("color: 0;")
-    #         self.cpuName_label.setStyleSheet("color: 0;")
-    #         self.gpuName_label.setStyleSheet("color: 0;")
-    #         self.AMD_logo.setPixmap(self.AMD_Radeon_pixmap)
-    #         if self.rali_checkBox.isChecked():
-    #             self.MIVisionX_logo.setPixmap(self.rali_pixmap)
-    #             self.graph_imageLabel.setPixmap(self.graph_image_pixmap)
-    #         else:
-    #             self.MIVisionX_logo.setPixmap(self.MIVisionX_pixmap)
-    #         self.EPYC_logo.setPixmap(self.EPYC_pixmap)
-    #         self.totalCurve.setData(x=self.x, y=self.y, pen=self.pen)
-
     def closeEvent(self, event):
         self.trainEngine.terminate()
-        self.receiver_thread.quit()
+        #self.receiver_thread.quit()
         self.close()
         exit(0)
